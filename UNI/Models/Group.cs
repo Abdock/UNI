e14786e9d4c2a0a4346fe5ffaa0951d06d8383bd
Abdusattar <abdusattar.kasymbekov@gmail.com>
2021-04-23 02:20:24 +0600

    Added 2 classes for work with db

---
+++ UNI/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UNI.Models
+{
+    public class Group
+    {
+        [Key]
+        public long group_id { get; set; }
+        public string group_name { get; set; }
+        public long curator_id { get; set; }
+    }
+}
---
+++ UNI/Models/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UNI.Models
+{
+    public class subject
+    {
+        [Key]
+        public long subject_id { get; set; }
+        public string subject_name { get; set; }
+    }
+}
